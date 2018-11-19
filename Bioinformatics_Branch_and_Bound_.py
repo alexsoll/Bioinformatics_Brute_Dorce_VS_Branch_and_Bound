b6e13120c7178465e7ac8a64cd631c0076ffb42b
alexsoll <al.soluyanov@rambler.ru>
2018-11-19 23:10:22 +0300

    Task#1 and Task2 is done!

--- Bioinformatics_Branch_and_Bound_.py
+++ Bioinformatics_Branch_and_Bound_.py
@@ -1,7 +1,6 @@
 import time
 
 def ParentMass(spectrum):
-    #spec = spectrum.split(" ")
     return int(max(spectrum))
 
 
@@ -19,14 +18,11 @@ def CycloSpectrum(peptide):
     total_sum = 0
     string = ''
     dict = Amino_Acid_Mass()
-    #l.append(str(0))
     l.append(0)
     for amino_acid in peptide:
         elem = dict.get(amino_acid)
         total_sum += elem
-        #l.append(str(elem))
         l.append(elem)
-    #l.append(str(total_sum))
     l.append(total_sum)
     ln = len(peptide)
     for i, val in enumerate(peptide):
@@ -38,15 +34,11 @@ def CycloSpectrum(peptide):
                 tmp = peptide[i:i+j+1]
             for char in tmp:
                 sum += dict.get(char)
-            #l.append(str(sum))
             l.append(sum)
     
     l.sort()
     return l
-    #string += str(l[0])
-    #for i in range(1,len(l)):
-    #    string += ' ' + str(l[i])
-    #return string
+
 
 def peptideMass(pept):
     dict = Amino_Acid_Mass()
@@ -54,6 +46,13 @@ def peptideMass(pept):
     for item in pept:
         sum += dict.get(item)
     return sum
+
+def Linear_Spectrum(peptide):
+    spectrum = [0]
+    for i, item in enumerate(peptide):
+        for window in range(len(peptide) - i):
+            spectrum.append(peptideMass(peptide[i:i + window + 1]))
+    return spectrum
 
 def Expand(peptides,spectrum):
     dict = Amino_Acid_Mass()
@@ -65,25 +64,13 @@ def Expand(peptides,spectrum):
             newSubList = list(item)
             newSubList.append(key)
             newlist.append(newSubList)
-            #newlist.append(item+key)
     return newlist
 
 
 def consistent(peptide, spectrum):
     dict = Amino_Acid_Mass()
-    #pept = []
-    #for i in range(len(peptide)):
-    #    pept.append(peptide[i:i+1])
-    #for j in range(len(pept)):
-    #    pept[j] = str(dict.get(pept[j]))
-    #spec = spectrum.split(" ")
-    #coincidence = 0
-    pept = list(peptide)
+    pept = Linear_Spectrum(peptide)
     spec = list(spectrum)
-    for i,val in enumerate(pept):
-        #pept[i] = str(dict.get(val))
-        pept[i] = dict.get(val)
-
     for item in pept:
         if item in spec:
             spec.remove(item)
@@ -115,31 +102,18 @@ def CyclopeptideSequencing(spectrum):
    result = []
    while len(peptides) != 0:
            peptides = Expand(peptides,spectrum)
-           #ln = (len(peptides))
-           #tmp = 0
-           #to_delete = []
-           #for peptide in peptides:
            ln = (len(peptides))
            tmp = 0
            for i in range(ln):
-           #for item in peptides:
                i -= tmp
-               #for j in peptides[i]:
-               #   peptide += j
                if peptideMass(peptides[i]) == parentmass:
                    if CycloSpectrum(peptides[i]) == spectrum:
-                        #print(peptide)
                         result.append(peptides[i])
                         peptides.remove(peptides[i])
                         tmp += 1
-                        #to_delete.append(peptide[i])
                elif not consistent(peptides[i], spectrum):
-                   #to_delete.append(peptides[i])
                    peptides.remove(peptides[i])
                    tmp += 1
-               #peptide = ''
-           #for item in to_delete:
-            #   peptides.remove(item)
    return result
 
 def Cyclopeptide_Scoring(peptide, spectrum):
@@ -158,12 +132,15 @@ start_time = time.time()
 
 #Cyclopeptide_Scoring('NQEL','0 99 113 114 128 227 257 299 355 356 370 371 484')
 
-#spectrum = '0 113 128 186 241 299 314 427'.split(" ")
-#spectrum = list(map(int,spectrum))
-spectrum = CycloSpectrum('LIWTVEG')
+spectrum = '0 113 128 186 241 299 314 427'.split(" ")
+spectrum = list(map(int,spectrum))
+#spectrum = CycloSpectrum('LIWTVEG')
 spectrum.sort()
 res = CyclopeptideSequencing(spectrum)
 print(peptide_to_mass(res))
+
+#consistent('IIEGTWW', spectrum)
+#print(CycloSpectrum('IIEGTWW') == spectrum)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
