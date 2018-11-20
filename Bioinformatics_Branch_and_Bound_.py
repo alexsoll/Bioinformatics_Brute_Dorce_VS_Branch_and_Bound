adb7393a5d20bc0ab67ef15e73e2a27d0faaf3ce
alexsoll <al.soluyanov@rambler.ru>
2018-11-20 21:03:05 +0300

    Task#3 is done

--- Bioinformatics_Branch_and_Bound_.py
+++ Bioinformatics_Branch_and_Bound_.py
@@ -1,4 +1,5 @@
 import time
+from collections import Counter
 
 def ParentMass(spectrum):
     return int(max(spectrum))
@@ -59,8 +60,6 @@ def Expand(peptides,spectrum):
     newlist = list()
     for item in peptides:
         for key in dict:
-            if not consistent(list(key), spectrum):
-                continue
             newSubList = list(item)
             newSubList.append(key)
             newlist.append(newSubList)
@@ -96,7 +95,6 @@ def peptide_to_mass(peptide):
 
 
 def CyclopeptideSequencing(spectrum):
-   #peptide = ''
    peptides = ['']
    parentmass = ParentMass(spectrum)
    result = []
@@ -128,19 +126,83 @@ def Cyclopeptide_Scoring(peptide, spectrum):
     print(score)
 
 
+#####################################################################
+
+
+def Score(peptide, spectrum):
+    theory_spec = Linear_Spectrum(peptide)
+    experimental_spectrum = list(spectrum)
+    score = 0
+    for item in experimental_spectrum:
+        if item in theory_spec:
+            theory_spec.remove(item)
+            score += 1
+    return score
+
+def Trim(LeaderBoard,spectrum, N):
+    if len(LeaderBoard) < N:
+        return LeaderBoard
+    score_list = []
+    for peptide in LeaderBoard:
+        score = Score(peptide,spectrum)
+        score_list.append(score)
+    score_list, LeaderBoard = (list(t) for t in zip(*sorted(zip(score_list, LeaderBoard), reverse = True)))
+    new_leaderBoard = LeaderBoard[:N]
+    for i in range(N,len(LeaderBoard)):
+        if (score_list[i] == score_list[N-1]):
+            new_leaderBoard.append(LeaderBoard[i])
+        else:
+            break
+    return new_leaderBoard 
+
+def LeaderBoardCyclopeptideSequencing(spectrum, N):
+    leaderBoard = ['']
+    LeaderPeptide = []
+    parentmass = ParentMass(spectrum)
+    while len(leaderBoard) != 0:
+        leaderBoard = Expand(leaderBoard,spectrum)
+        ln = len(leaderBoard)
+        tmp = 0
+        for i in range(ln):
+            i -= tmp
+            if peptideMass(leaderBoard[i]) == parentmass:
+                if Score(leaderBoard[i], spectrum) > Score(LeaderPeptide, spectrum):
+                    LeaderPeptide = list(leaderBoard[i])
+            elif peptideMass(leaderBoard[i]) > parentmass:
+                leaderBoard.remove(leaderBoard[i])
+                tmp += 1
+        leaderBoard = Trim(leaderBoard,spectrum, N)
+    return LeaderPeptide
+
+def print_mass(peptide):
+    dict = Amino_Acid_Mass()
+    result = ''
+    for amino_acid in peptide:
+        result += str(dict.get(amino_acid)) + '-'
+    result = result[:len(result)-1]
+    return result
+
 start_time = time.time()
 
 #Cyclopeptide_Scoring('NQEL','0 99 113 114 128 227 257 299 355 356 370 371 484')
 
-spectrum = '0 113 128 186 241 299 314 427'.split(" ")
-spectrum = list(map(int,spectrum))
+#spectrum = '0 113 128 186 241 299 314 427'.split(" ")
+#spectrum = list(map(int,spectrum))
 #spectrum = CycloSpectrum('LIWTVEG')
-spectrum.sort()
-res = CyclopeptideSequencing(spectrum)
-print(peptide_to_mass(res))
+#spectrum.sort()
+#res = CyclopeptideSequencing(spectrum)
+#print(peptide_to_mass(res))
+
+spectrum = '0 71 101 103 113 114 128 131 156 156 172 199 232 242 259 269 270 287 300 303 313 372 372 373 388 398 400 414 431 459 469 486 501 501 503 528 545 570 572 572 587 604 614 642 659 673 675 685 700 701 701 760 770 773 786 803 804 814 831 841 857 874 901 917 917 942 945 959 960 970 972 1002 1073'.split(" ")
+
+
+N = 10
+#spectrum = '0 71 113 129 147 200 218 260 313 331 347 389 460'.split(" ")
+spectrum = list(map(int, spectrum))
+result = LeaderBoardCyclopeptideSequencing(spectrum, N)
+print(print_mass(result))
 
 #consistent('IIEGTWW', spectrum)
 #print(CycloSpectrum('IIEGTWW') == spectrum)
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
